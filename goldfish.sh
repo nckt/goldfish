dcb55265cef21b6e044474f5fab34eeb09ab0a71
nckt <goldfish@www6217uo.sakura.ne.jp>
2014-10-13 07:55:24 +0900

    とりあえず完成。

--- goldfish.sh
+++ goldfish.sh
@@ -14,11 +14,9 @@ HTMLDIR=$WORKDIR/html
 # @param 更新があったページの取得結果
 # @param 更新があったページのURL
 ##
-send_mail(){
-	local recipient=cat ${RECIPIENTFILE} | jq '.[]' | perl -pe 's/\n/,/'
-	local title=`cat $1 \
-		| awk 'match($0, /<title>.*?<\/title>/)'{print substr($0, RSTART, RLENGTH) \
-		| sed s/<title>|<\/title>//`
+send_mail() {
+	local recipient=`cat ${RECIPIENTFILE} | jq '.[]' | perl -pe 's/\n/,/'`
+	local title=`cat $1 | awk 'match($0, /<title>.*?<\/title>/){print substr(\$0, RSTART, RLENGTH)}' | sed s/\<title\>\|\<\\\/title\>//g`
 	cat << EOT | mail -s '[goldfish]サイト更新通知' ${recipient}
 サイトが更新されました!
 
@@ -32,16 +30,21 @@ EOT
 # --- 本体処理 --- #
 for SITE in $(ls -1 ${SITESDIR}/*.json); do
 	# 前回の取得結果をリネームして残しておく
-	HTMLFILENAME=`cat ${SITE} | jq '.id'`.html
+	HTMLFILENAME=`cat ${SITE} | jq '.id' | sed s/\"//g`.html
 	HTMLFULLPATH=${HTMLDIR}/${HTMLFILENAME}
-	cp ${HTMLFULLPATH} ${HTMLFULLPATH}.old
+	if [ -f ${HTMLFULLPATH} ]; then
+		mv ${HTMLFULLPATH} ${HTMLFULLPATH}.old
+	else
+		touch ${HTMLFULLPATH}.old
+	fi
 	
 	# 現在と前回を比較し、違いがあったらメールする
-	URL=`cat SITE | jq '.url'`
-	curl -o ${FILENAME} ${URL}
+	URL=`cat ${SITE} | jq '.url' | sed s/\"//g`
+	curl ${URL} | perl -pe 's/\n//' | sed s/tdftad.*// > ${HTMLFULLPATH}
 	RESULT=`diff ${HTMLFULLPATH} ${HTMLFULLPATH}.old | wc -l`
 	if [ ${RESULT} > 0 ]; then
 		send_mail ${HTMLFULLPATH} ${URL}
+		echo "メールを送信しました。"
 	fi
 done
 
---
+++ test/test.bats
@@ -0,0 +1,36 @@
+#!/usr/bin/env bats
+
+###
+# goldfishのテスト
+###
+
+setup(){
+	NOW=`date +%y%m%d%H%M%S`
+	cd `dirname $0`
+	cd ../config/sites
+	for FILE in *.json; do
+		mv ${FILE} ${FILE}.bak
+	done
+
+	echo '{id:"test'${NOW}'",url:"http://example.com"' > ${NOW}.json
+}
+
+teardown(){
+	cd `dirname $0`
+	cd ../config/sites
+	for FILE in *.json; do
+		mv ${FILE}.bak ${FILE}
+	done
+	rm ${NOW}.json
+}
+
+@test "一回目の実行時はメールが送信される" {
+	run ../goldfish.sh
+	[ $line[0] = "メールを送信しました。" ]
+}
+
+@test "二回目の実行時はメールが送信されない" {
+	run ../goldfish.sh
+	[ $output = "" ]
+}
+
