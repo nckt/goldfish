f852fb98f9a77dda6b2c279e087d37a13ebccaff
nakakita junsuke <galikuson@gmail.com>
2014-10-13 19:00:35 +0900

    コマンド内の改行をなくした

--- goldfish.sh
+++ goldfish.sh
@@ -16,9 +16,7 @@ HTMLDIR=$WORKDIR/html
 ##
 send_mail(){
 	local recipient=cat ${RECIPIENTFILE} | jq '.[]' | perl -pe 's/\n/,/'
-	local title=`cat $1 \
-		| awk 'match($0, /<title>.*?<\/title>/)'{print substr($0, RSTART, RLENGTH) \
-		| sed s/<title>|<\/title>//`
+	local title=`cat $1 | awk 'match($0, /<title>.*?<\/title>/)'{print substr($0, RSTART, RLENGTH) | sed s/<title>|<\/title>//`
 	cat << EOT | mail -s '[goldfish]サイト更新通知' ${recipient}
 サイトが更新されました!
 
