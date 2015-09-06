ba316a4f5b0e857075b1588c2cc0c0a3209d334f
junsuke nakakita <junsuke@www6217uo.sakura.ne.jp>
2015-09-06 17:42:12 +0900

    とりあえず動くようにした。

--- goldfish.sh
+++ goldfish.sh
@@ -11,38 +11,74 @@ HTMLDIR=$WORKDIR/html
 ##
 # 更新通知メールを送信
 #
-# @param 更新があったページの取得結果
+# @param 更新があったページのID
 # @param 更新があったページのURL
 ##
 send_mail(){
-	local recipient=cat ${RECIPIENTFILE} | jq '.[]' | perl -pe 's/\n/,/'
-	local title=`cat $1 \
-		| awk 'match($0, /<title>.*?<\/title>/)'{print substr($0, RSTART, RLENGTH) \
-		| sed s/<title>|<\/title>//`
+	local recipient=`cat ${RECIPIENTFILE} | jq '.[]' | perl -pe 's/\n/,/'`
+
+	echo "mail -s '[goldfish]サイト更新通知' ${recipient}"
 	cat << EOT | mail -s '[goldfish]サイト更新通知' ${recipient}
 サイトが更新されました!
 
-${title}
+Site id: ${1}
 ${2}
 --
-I'm goldfish.
+goldfish
 EOT
+	return ${?}
 }
 
 # --- 本体処理 --- #
 for SITE in $(ls -1 ${SITESDIR}/*.json); do
+	ID=`cat ${SITE} | jq -r '.id'`
+	URL=`cat ${SITE} | jq -r '.url'`
+	REGEXP=`cat ${SITE} | jq -r '.regexp'`
+	IS_REGEXP_MULTILINE=`cat ${SITE} | jq '.is_regexp_multiline'`
+	COMMAND=`cat ${SITE} | jq -r '.pipeline_command'`
+
+	echo "----- CHECK ${ID} -----"
+
 	# 前回の取得結果をリネームして残しておく
-	HTMLFILENAME=`cat ${SITE} | jq '.id'`.html
+	HTMLFILENAME=${ID}.html
 	HTMLFULLPATH=${HTMLDIR}/${HTMLFILENAME}
+	touch ${HTMLFULLPATH}.old
 	cp ${HTMLFULLPATH} ${HTMLFULLPATH}.old
 	
 	# 現在と前回を比較し、違いがあったらメールする
-	URL=`cat SITE | jq '.url'`
-	curl -o ${FILENAME} ${URL}
-	RESULT=`diff ${HTMLFULLPATH} ${HTMLFULLPATH}.old | wc -l`
-	if [ ${RESULT} > 0 ]; then
-		send_mail ${HTMLFULLPATH} ${URL}
-		echo "メールを送信しました。"
+	curl -o ${HTMLFULLPATH} ${URL}
+
+	if [ "${IS_REGEXP_MULTILINE}" = 'true' ]; then
+		echo '[TRACE] REGEXP MULTI LINE MODE IS ENABLE. DELETE LINE BREAKS.'
+		cat ${HTMLFULLPATH} | tr -d '\n' > ${HTMLFULLPATH}.tmp
+		mv -f ${HTMLFULLPATH}.tmp ${HTMLFULLPATH}
 	fi
+
+	if [ "${REGEXP}" != 'null' ]; then
+		echo '[TRACE] REGEXP IS SET. MATCH REGEXP.'
+		cat ${HTMLFULLPATH} | grep -o -P "${REGEXP}" > ${HTMLFULLPATH}.tmp
+		mv -f ${HTMLFULLPATH}.tmp ${HTMLFULLPATH}
+	fi
+
+	if [ "${COMMAND}" != 'null' ]; then
+		echo '[TRACE] PIPE LINE COMMAND IS SET. EXECUTE COMMAND.'
+		cat ${HTMLFULLPATH} | ${COMMAND} > ${HTMLFULLPATH}.tmp
+		mv -f ${HTMLFULLPATH}.tmp ${HTMLFULLPATH}
+	fi
+
+	RESULT=`diff ${HTMLFULLPATH} ${HTMLFULLPATH}.old | wc -l`
+	if [ ${RESULT} -gt 0 ]; then
+		send_mail ${ID} ${URL}
+
+		if [ ${?} -eq 0 ]; then
+			echo "[TRACE] MAIL SENT."
+		else
+			echo '[ERROR!] MAIL SEND FAILURE.'
+		fi
+	else
+		echo '[TRACE] THERE ARE NO CHANGES.'
+	fi
+
+	echo "--- CHECK ${ID} END ---"
 done
 
